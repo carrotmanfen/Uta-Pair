51b6b99f0057715447db809f9d21a5258f0acefe
carrotmanfen <kudsun.khamjamnun@mail.kmutt.ac.th>
2022-10-08 15:49:23 +0700

    btn play in java

--- app/src/main/java/com/example/utapair/MainActivity.java
+++ app/src/main/java/com/example/utapair/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.utapair;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -17,20 +18,29 @@ import com.example.utapair.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button buttonPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-
-
+        buttonPlay = (Button) findViewById(R.id.play_btn);
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSelectLevelActivity();
+            }
+        });
     }
 
+    public void openSelectLevelActivity(){
+        Intent intent=new Intent(this, SelectLevelActivity.class);
+        startActivity(intent);
+    }
 
 
 }
