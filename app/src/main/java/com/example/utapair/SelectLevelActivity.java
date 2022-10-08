a134c6ad9f20990449cc40663e058794ecf79925
carrotmanfen <kudsun.khamjamnun@mail.kmutt.ac.th>
2022-10-08 15:29:27 +0700

    New UI about select level

--- app/src/main/java/com/example/utapair/SelectLevelActivity.java
+++ app/src/main/java/com/example/utapair/SelectLevelActivity.java
@@ -3,9 +3,10 @@ package com.example.utapair;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SelectLevelActivity extends AppCompatActivity {
-
+    private Button buttonPlay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
