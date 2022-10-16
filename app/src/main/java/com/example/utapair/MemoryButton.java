c3afa810fc0201f39519949e2dfc2fef6f1babf0
MangTps <thanpisit.pisitpon@mail.kmutt.ac.th>
2022-10-16 21:41:17 +0700

    Comment GameActivity.java,MemoryButton.java,MenuActivity.java and  add android theme on AndroidManifest.xml

--- app/src/main/java/com/example/utapair/MemoryButton.java
+++ app/src/main/java/com/example/utapair/MemoryButton.java
@@ -1,65 +1,65 @@
-package com.example.utapair;
-
-import android.content.Context;
-import android.graphics.drawable.Drawable;
-import android.widget.GridLayout;
-
-import androidx.appcompat.widget.AppCompatButton;
-
-
-public class MemoryButton extends AppCompatButton {
-    // attributes
-    protected int row;
-    protected int col;
-    protected int frontDrawableId;
-
-    protected boolean isFilpped = false;
-    protected boolean isMatched = false;
-
-    protected Drawable front;
-    protected Drawable back;
-
-    // constructor
-    public MemoryButton(Context context, int r, int c, int frontImageId){ // give position(r,c) and id of image in drawable source of gridlayout first
-        super(context);
-
-        row = r;
-        col = c;
-        frontDrawableId = frontImageId; // declare id picture for known when they matching
-
-        front = context.getDrawable(frontImageId);
-        back = context.getDrawable(R.drawable.รูปที่ต้องวาดใน folder drawable); // ตรงนี้ต้องใส่ไปเลยให้ findviewbyId ของรูป background
-
-        setBackground(back);
-
-        GridLayout.LayoutParams tempParams = new GridLayout.LayoutParams(GridLayout.spec(r),GridLayout.spec(c)); // it tell position of grid
-
-        tempParams.width = (int) getResources().getDisplayMetrics().density * 50;
-        tempParams.height = (int) getResources().getDisplayMetrics().density * 50;
-
-        setLayoutParams(tempParams);
-    }
-    public boolean isMatched(){
-        return  isMatched;
-    }
-    public void setMatched(boolean isMatched){
-        this.isMatched = isMatched;
-    }
-    public int getFrontDrawableId(){
-        return frontDrawableId;
-    }
-    public void filpped(){
-        if(isMatched){
-            return;
-        }
-        if(isFilpped){ // true -> filpped
-            setBackground(back);
-            isFilpped = false;
-        }
-        else { // false -> flipped
-            setBackground(front);
-            isFilpped = true;
-        }
-    }
-
-}
+//package com.example.utapair;
+//
+//import android.content.Context;
+//import android.graphics.drawable.Drawable;
+//import android.widget.GridLayout;
+//
+//import androidx.appcompat.widget.AppCompatButton;
+//
+//
+//public class MemoryButton extends AppCompatButton {
+//    // attributes
+//    protected int row;
+//    protected int col;
+//    protected int frontDrawableId;
+//
+//    protected boolean isFilpped = false;
+//    protected boolean isMatched = false;
+//
+//    protected Drawable front;
+//    protected Drawable back;
+//
+//    // constructor
+//    public MemoryButton(Context context, int r, int c, int frontImageId){ // give position(r,c) and id of image in drawable source of gridlayout first
+//        super(context);
+//
+//        row = r;
+//        col = c;
+//        frontDrawableId = frontImageId; // declare id picture for known when they matching
+//
+//        front = context.getDrawable(frontImageId);
+//        back = context.getDrawable(R.drawable.รูปที่ต้องวาดใน folder drawable); // ตรงนี้ต้องใส่ไปเลยให้ findviewbyId ของรูป background
+//
+//        setBackground(back);
+//
+//        GridLayout.LayoutParams tempParams = new GridLayout.LayoutParams(GridLayout.spec(r),GridLayout.spec(c)); // it tell position of grid
+//
+//        tempParams.width = (int) getResources().getDisplayMetrics().density * 50;
+//        tempParams.height = (int) getResources().getDisplayMetrics().density * 50;
+//
+//        setLayoutParams(tempParams);
+//    }
+//    public boolean isMatched(){
+//        return  isMatched;
+//    }
+//    public void setMatched(boolean isMatched){
+//        this.isMatched = isMatched;
+//    }
+//    public int getFrontDrawableId(){
+//        return frontDrawableId;
+//    }
+//    public void filpped(){
+//        if(isMatched){
+//            return;
+//        }
+//        if(isFilpped){ // true -> filpped
+//            setBackground(back);
+//            isFilpped = false;
+//        }
+//        else { // false -> flipped
+//            setBackground(front);
+//            isFilpped = true;
+//        }
+//    }
+//
+//}
--- app/src/main/java/com/example/utapair/MenuActivity.java
+++ app/src/main/java/com/example/utapair/MenuActivity.java
@@ -1,23 +1,23 @@
-package com.example.utapair;
-
-import android.os.Bundle;
-import android.widget.Button;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MenuActivity extends AppCompatActivity {
-
-    private Button easyButton; // dimension = ? 2 rows , 3 cols
-    private Button mediumButton; // dimension = ?
-    private Button HardButton; // dimension = ?
-
-    @Override
-    protected void onCreate(Bundle saveInstanceState){
-        super.onCreate(saveInstanceState);
-        setContentView(R.layout.activity_menu); // หน้า menu file layout
-
-
-        // if you wanna try new method just learn it
-    }
-
-}
+//package com.example.utapair;
+//
+//import android.os.Bundle;
+//import android.widget.Button;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//public class MenuActivity extends AppCompatActivity {
+//
+//    private Button easyButton; // dimension = ? 2 rows , 3 cols
+//    private Button mediumButton; // dimension = ?
+//    private Button HardButton; // dimension = ?
+//
+//    @Override
+//    protected void onCreate(Bundle saveInstanceState){
+//        super.onCreate(saveInstanceState);
+//        setContentView(R.layout.activity_menu); // หน้า menu file layout
+//
+//
+//        // if you wanna try new method just learn it
+//    }
+//
+//}
