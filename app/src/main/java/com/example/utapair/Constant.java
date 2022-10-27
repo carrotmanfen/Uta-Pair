a572258ab12251dc3e824a8899bcf838445c3cc9
MangTps <thanpisit.pisitpon@mail.kmutt.ac.th>
2022-10-27 15:08:10 +0700

    Comment GameActivity.java,MemoryButton.java,MenuActivity.java and  add android theme on AndroidManifest.xml

---
+++ app/src/main/java/com/example/utapair/Constant.java
@@ -0,0 +1,8 @@
+package com.example.utapair;
+
+public class Constant {
+
+    public static final String ROOT_URL ="http://192.168.1.175/database_uta_pair/v1/register.php";
+
+    public static final String URL_REGISTER = ROOT_URL+"Register.php";
+}
