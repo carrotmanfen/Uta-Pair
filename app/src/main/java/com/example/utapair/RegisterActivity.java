adfe1869b36eed72b679462173147da3366f6dfa
MangTps <thanpisit.pisitpon@mail.kmutt.ac.th>
2022-10-27 15:07:45 +0700

    Comment GameActivity.java,MemoryButton.java,MenuActivity.java and  add android theme on AndroidManifest.xml

--- app/src/main/java/com/example/utapair/RegisterActivity.java
+++ app/src/main/java/com/example/utapair/RegisterActivity.java
@@ -1,14 +1,102 @@
 package com.example.utapair;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextUsername, editTextPassword, editTextConfirmPassword;
+    private Button buttonSignup;
+    private ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        editTextUsername = (EditText) findViewById(R.id.edit_text_username);
+        editTextPassword = (EditText) findViewById(R.id.edit_text_password);
+        editTextConfirmPassword = (EditText) findViewById(R.id.edit_text_confirm_password);
+
+
+        buttonSignup =(Button) findViewById(R.id.sign_up_btn);
+        progressDialog = new ProgressDialog(this);
+
+        buttonSignup.setOnClickListener(this);
+    }
+
+    private void registerUser(){
+        String username = editTextUsername.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        progressDialog.setMessage("Registering user...");
+        progressDialog.show();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                Constant.URL_REGISTER,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+                        try {
+                             JSONObject jsonObject = new JSONObject(response);
+
+                             Toast.makeText(getApplicationContext(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+
+
+                        } catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.hide();
+                        Toast.makeText(getApplicationContext(), error.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                }
+
+        ){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("username",username);
+                params.put("password",password);
+
+
+                return  params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+     }
+
+    @Override
+    public void onClick(View view) {
+        if(view == buttonSignup)
+            registerUser();
     }
 }
