0b7b0277413b75ba6a39dfb9198d6c28ce52a64d
carrotmanfen <kudsun.khamjamnun@mail.kmutt.ac.th>
2022-10-28 22:38:21 +0700

    game activity

--- app/src/main/java/com/example/utapair/EndgameActivity.java
+++ app/src/main/java/com/example/utapair/EndgameActivity.java
@@ -1,6 +1,13 @@
 package com.example.utapair;
 
 import android.app.Activity;
+import android.os.Bundle;
+
 
 public class EndgameActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_game);
+    }
 }
