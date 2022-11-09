407f657933a5766d4d0c57d0d3086c83477b9f33
Sorathorn Kaewchotchuangkul <sarathorn.16@gmail.com>
2022-11-09 21:28:38 +0700

    accessibility feature.

---
+++ app/src/main/java/com/example/utapair/MyAccessibilityService.java
@@ -0,0 +1,17 @@
+package com.example.utapair;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class MyAccessibilityService extends AccessibilityService {
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+    }
+
+    @Override
+    public void onInterrupt() {
+    }
+
+
+}
