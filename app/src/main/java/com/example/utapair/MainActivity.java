db05c0b9a940073958deea513e0fefc51db17e1a
carrotmanfen <kudsun.khamjamnun@mail.kmutt.ac.th>
2022-11-11 00:56:37 +0700

    MainActivity finish

--- app/src/main/java/com/example/utapair/MainActivity.java
+++ app/src/main/java/com/example/utapair/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Handler;
@@ -32,13 +33,89 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton buttonProfile;
     private ImageButton buttonScoreboard;
     private ImageButton buttonSetting;
-    private TextToSpeech t1;
-    int i = 0;
+    private TextToSpeech textToSpeak;
+    private int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        settingAll();
+
+        textToSpeak = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if (i != TextToSpeech.ERROR)
+                    textToSpeak.setLanguage(Locale.US);
+            }
+        });
+
+        buttonPlay = (Button) findViewById(R.id.play_btn);
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(AccessibilityMode.getInstance().getMode()=="ACCESSIBILITY") {
+                    openSelectLevelActivityAccessibility();
+                }
+                else{
+                    openSelectLevelActivity();
+                }
+            }
+        });
+
+        buttonProfile = (ImageButton) findViewById(R.id.profile_btn);
+        buttonProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(AccessibilityMode.getInstance().getMode()=="ACCESSIBILITY") {
+                    openAccountActivityAccessibility();
+                }
+                else{
+                    openAccountActivity();
+                }
+            }
+        });
+
+        buttonScoreboard = (ImageButton) findViewById(R.id.scoreboard_btn);
+        buttonScoreboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(AccessibilityMode.getInstance().getMode()=="ACCESSIBILITY") {
+                    openScoreboardActivityAccessibility();
+                }
+                else {
+                    openScoreboardActivity();
+                }
+            }
+        });
+
+        buttonSetting = (ImageButton) findViewById(R.id.setting_btn);
+        buttonSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(AccessibilityMode.getInstance().getMode()=="ACCESSIBILITY"){
+                    openSettingActivityAccessibility();
+                }
+                else {
+                    openSettingActivity();
+                }
+            }
+        });
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                String text = "Home";
+                textToSpeak.speak(text,TextToSpeech.QUEUE_FLUSH,null);
+            }
+        },500);
+    }
+
+    public void settingAll(){
         boolean checkedAccessibilityMode = PreferenceManager.getDefaultSharedPreferences(this)
                 .getBoolean("ACCESSIBILITY_CHECKBOX",false);
         boolean checkedBlindMode = PreferenceManager.getDefaultSharedPreferences(this)
@@ -55,75 +132,6 @@ public class MainActivity extends AppCompatActivity {
         else{
             BlindMode.getInstance().setMode("NOT_BLIND");
         }
-
-
-        final MediaPlayer buttonSoundClick = MediaPlayer.create(this,R.raw.correct);
-
-        t1 = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int i) {
-                if (i != TextToSpeech.ERROR)
-                    t1.setLanguage(Locale.US);
-            }
-        });
-
-        buttonPlay = (Button) findViewById(R.id.play_btn);
-        buttonPlay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(AccessibilityMode.getInstance().getMode()=="ACCESSIBILITY") {
-                    i++;
-                    Handler handler = new Handler();
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            if (i==1){
-                                String text = "Play";
-                                t1.speak(text,TextToSpeech.QUEUE_FLUSH,null);
-                            }
-                            else if(i==2){
-//                                String text = "Select level";
-//                                t1.speak(text,TextToSpeech.QUEUE_FLUSH,null);
-                                openSelectLevelActivity();
-                            }
-                            i=0;
-                        }
-                    },500);
-
-                    //buttonSoundClick.start();
-                   // String text = "Play";
-                    //t1.speak(text,TextToSpeech.QUEUE_FLUSH,null);
-                }
-                else{
-                    openSelectLevelActivity();
-                }
-                //openSelectLevelActivity();
-            }
-        });
-
-        buttonProfile = (ImageButton) findViewById(R.id.profile_btn);
-        buttonProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openAccountActivity();
-            }
-        });
-
-        buttonScoreboard = (ImageButton) findViewById(R.id.scoreboard_btn);
-        buttonScoreboard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openScoreboardActivity();
-            }
-        });
-
-        buttonSetting = (ImageButton) findViewById(R.id.setting_btn);
-        buttonSetting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openSettingActivity();
-            }
-        });
     }
 
     public void openSelectLevelActivity(){
@@ -131,9 +139,45 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void openSelectLevelActivityAccessibility(){
+        i++;
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (i==1){
+                    String text = "double tap to play";
+                    textToSpeak.speak(text,TextToSpeech.QUEUE_FLUSH,null);
+                }
+                else if(i==2){
+                    openSelectLevelActivity();
+                }
+                i=0;
+            }
+        },500);
+    }
+
     public void openAccountActivity(){
         Intent intent=new Intent(this, AccountActivity.class);
         startActivity(intent);
+    }
+
+    public void openAccountActivityAccessibility(){
+        i++;
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (i==1){
+                    String text = "double tap to go to profile";
+                    textToSpeak.speak(text,TextToSpeech.QUEUE_FLUSH,null);
+                }
+                else if(i==2){
+                    openAccountActivity();
+                }
+                i=0;
+            }
+        },500);
     }
 
     public void openScoreboardActivity(){
@@ -141,9 +185,45 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void openScoreboardActivityAccessibility(){
+        i++;
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (i==1){
+                    String text = "double tap to go to scoreboard";
+                    textToSpeak.speak(text,TextToSpeech.QUEUE_FLUSH,null);
+                }
+                else if(i==2){
+                    openScoreboardActivity();
+                }
+                i=0;
+            }
+        },500);
+    }
+
     public void openSettingActivity(){
         Intent intent=new Intent(this, SettingActivity.class);
         startActivity(intent);
+    }
+
+    public void openSettingActivityAccessibility(){
+        i++;
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (i==1){
+                    String text = "double tap to go to setting";
+                    textToSpeak.speak(text,TextToSpeech.QUEUE_FLUSH,null);
+                }
+                else if(i==2){
+                    openSettingActivity();
+                }
+                i=0;
+            }
+        },500);
     }
 
 }
