22f74b09181791185fd22bcc8339b799b13f91aa
Fay23145 <Kanthila.23145@mail.kmutt.ac.th>
2022-11-12 00:46:51 +0700

    it very hard

--- app/src/main/java/com/example/utapair/LoginActivity.java
+++ app/src/main/java/com/example/utapair/LoginActivity.java
@@ -32,7 +32,7 @@ public class LoginActivity extends AppCompatActivity {
     private Button buttonLogin;
     private ImageButton buttonBack;
 //  Connect Server
-    private String URL = "https://ac32-2001-3c8-1406-35-244-00-29a.ap.ngrok.io/UTA/checkLogin.php";
+    private String URL = "https://f40b-124-120-4-216.ap.ngrok.io/UTA/checkLogin.php";
 
     @SuppressLint("MissingInflatedId")
     @Override
